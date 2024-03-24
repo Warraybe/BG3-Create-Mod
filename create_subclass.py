47d3e1834d27f1bd103ebc96e37497474fcbd8b9
Raymond Opella <ray.opella@gmail.com>
2024-03-24 18:58:31 -0500

    Remove redundant imports and update file creation.

--- create_subclass.py
+++ create_subclass.py
@@ -1,10 +1,6 @@
 from class_info import base_classes
-from create_description import create_description
+from create_files import create_files
 from create_folders import create_folders
-from create_localization import create_localization
-from create_meta import create_meta
-from create_progression import create_progression
-from create_sciptextender_files import create_scriptextender_files
 from create_uuids import create_uuids
 
 mod_author = input("Mod author name: ").title()
@@ -19,9 +15,6 @@ mod_dir = f'{mod_author.replace(" ", "")}{subclass.replace(" ", "")}'
 mod_info = {"mod_author": mod_author, "subclass": subclass,
             "main_class": base_class, "mod_name": mod_dir, "mod_dir": mod_dir}
 uuids = create_uuids(base_class)
+
 create_folders(mod_info)
-create_meta(mod_info, uuids)
-create_scriptextender_files(mod_info, uuids)
-create_localization(mod_info, uuids)
-create_description(mod_info, uuids)
-create_progression(mod_info, uuids)
+create_files(mod_info, uuids)
