a7e72f5979b176e6787e47ef0d18b5d58d7c12dd
Raymond Opella <ray.opella@gmail.com>
2024-03-27 17:36:24 -0500

    Remove references to class mod to focus file on subclass only.

--- create_subclass.py
+++ create_subclass.py
@@ -3,29 +3,24 @@ from create_files import create_subclass_files
 from create_folders import create_folders
 from create_uuids import create_subclass_uuids
 
-mod_type = input("Type of mod (class/subclass): ").lower()
-mod_author = input("Mod author name: ").title()
-base_class = ""
-while base_class not in base_classes.keys():
-    base_class = input("Base class: ").lower()
-    if base_class not in base_classes.keys():
-        print(f"Please enter a class from {', '.join([*base_classes])}")
-subclass = input("Subclass name: ").title()
 
-mod_dir = f'{mod_author.replace(" ", "")}{subclass.replace(" ", "")}'
-mod_info = {
-    "mod_author": mod_author,
-    "subclass": subclass,
-    "main_class": base_class,
-    "mod_name": mod_dir,
-    "mod_dir": mod_dir,
-}
-uuids = create_subclass_uuids(base_class)
+def create_subclass_mod():
+    mod_author = input("Mod author name: ").title()
+    base_class = ""
+    while base_class not in base_classes.keys():
+        base_class = input("Base class: ").lower()
+        if base_class not in base_classes.keys():
+            print(f"Please enter a class from {', '.join([*base_classes])}")
+    subclass = input("Subclass name: ").title()
 
-if mod_type == "subclass":
+    mod_dir = f'{mod_author.replace(" ", "")}{subclass.replace(" ", "")}'
+    mod_info = {
+        "mod_author": mod_author,
+        "subclass": subclass,
+        "main_class": base_class,
+        "mod_name": mod_dir,
+        "mod_dir": mod_dir,
+    }
+    uuids = create_subclass_uuids(base_class)
     create_folders(mod_info)
     create_subclass_files(mod_info, uuids)
-elif mod_type == "class":
-    pass
-else:
-    print("No mod created.")
