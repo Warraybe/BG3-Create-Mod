025648b641bd3cec203724904fffdefbf8df4c1a
Raymond Opella <ray.opella@gmail.com>
2024-03-27 18:11:15 -0500

    Add class creation function call.

--- create_mod.py
+++ create_mod.py
@@ -1,10 +1,11 @@
 from create_subclass import create_subclass_mod
+from create_class import create_class_mod
 
 mod_type = input("Type of mod (class/subclass): ").lower()
 
 if mod_type == "subclass":
     create_subclass_mod()
 elif mod_type == "class":
-    pass
+    create_class_mod()
 else:
     print("No mod created.")
