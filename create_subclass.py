c131f87d8daae6e72a5a30d8fb3f58396ce4cdcc
Raymond Opella <ray.opella@gmail.com>
2024-03-27 17:28:34 -0500

    Correct formatting and add initial steps towards class creation feature.

--- create_subclass.py
+++ create_subclass.py
@@ -3,6 +3,7 @@ from create_files import create_subclass_files
 from create_folders import create_folders
 from create_uuids import create_subclass_uuids
 
+mod_type = input("Type of mod (class/subclass): ").lower()
 mod_author = input("Mod author name: ").title()
 base_class = ""
 while base_class not in base_classes.keys():
@@ -12,9 +13,19 @@ while base_class not in base_classes.keys():
 subclass = input("Subclass name: ").title()
 
 mod_dir = f'{mod_author.replace(" ", "")}{subclass.replace(" ", "")}'
-mod_info = {"mod_author": mod_author, "subclass": subclass,
-            "main_class": base_class, "mod_name": mod_dir, "mod_dir": mod_dir}
+mod_info = {
+    "mod_author": mod_author,
+    "subclass": subclass,
+    "main_class": base_class,
+    "mod_name": mod_dir,
+    "mod_dir": mod_dir,
+}
 uuids = create_subclass_uuids(base_class)
 
-create_folders(mod_info)
-create_subclass_files(mod_info, uuids)
+if mod_type == "subclass":
+    create_folders(mod_info)
+    create_subclass_files(mod_info, uuids)
+elif mod_type == "class":
+    pass
+else:
+    print("No mod created.")
