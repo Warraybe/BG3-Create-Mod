1eab014e7885bb323675a08d6bd90106b5e4eace
Raymond Opella <ray.opella@gmail.com>
2024-03-27 18:28:41 -0500

    Clean up mod information input and creation.

--- create_subclass.py
+++ create_subclass.py
@@ -1,25 +1,21 @@
-from class_info import base_classes
+from class_info import main_classes
 from create_files import create_subclass_files
 from create_folders import create_folders
 from create_uuids import create_subclass_uuids
 
 
 def create_subclass_mod():
-    mod_author = input("Mod author name: ").title()
-    base_class = ""
-    while base_class not in base_classes.keys():
-        base_class = input("Base class: ").lower()
-        if base_class not in base_classes.keys():
-            print(f"Please enter a class from {', '.join([*base_classes])}")
-    subclass = input("Subclass name: ").title()
+    mod_info = {"mod_author": input("Mod author name: ").title(), "main_class": ""}
+    while mod_info["main_class"] not in main_classes.keys():
+        mod_info["main_class"] = input("Main class: ").lower()
+        if mod_info["main_class"] not in main_classes.keys():
+            print(f"Please enter a class from {', '.join([*main_classes])}")
+    mod_info["subclass"] = input("Subclass name: ").title()
 
-    mod_dir = f'{mod_author.replace(" ", "")}{subclass.replace(" ", "")}'
-    mod_info = {
-        "mod_author": mod_author,
-        "subclass": subclass,
-        "main_class": base_class,
-        "mod_name": mod_dir,
-    }
-    uuids = create_subclass_uuids(base_class)
+    mod_info[
+        "mod_name"
+    ] = f'{mod_info["mod_author"].replace(" ", "")}{mod_info["subclass"].replace(" ", "")}'
+
+    uuids = create_subclass_uuids(mod_info["main_class"])
     create_folders(mod_info)
     create_subclass_files(mod_info, uuids)
